ef14158f3445c09a2e51d00cf014251ac7941b63
zengbing15 <zengb@cryptape.com>
2021-08-03 12:00:19 +0800

    initialize web app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes');
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
+app.use(routes);
+
+app.listen(app.get('port'), function() {
+    console.log('Server started on port' + app.get('port'));
+})
